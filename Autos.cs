a72663728dae195a358fe6ba0e400269d95815b4
Pawel Alessandro Reynoso Marquez <pawelreynoso@gmail.com>
2022-03-10 17:14:14 -0600

    Clases agregdas: Clientes (para datos de clientes) y Autos (datos de autos alquilados)

---
+++ Autos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoLab6
+{
+    internal class Autos
+    {
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Recorrido { get; set; }
+    }
+}
---
+++ Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoLab6
+{
+    internal class Clientes
+    {
+        public string NIT { get; set; }
+        public string Nombre { get; set; }
+        public string Dirección { get; set; }
+    }
+}
